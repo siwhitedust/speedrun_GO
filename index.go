4357945ed09e3010d7ef66dc96556dec0e15eefa
siwhitedust <stickgaming27@gmail.com>
2025-09-16 13:32:49 +0700

    update sintaks dasar

--- index.go
+++ index.go
@@ -1,8 +1,25 @@
-package speedrun_GO
+package main
 
 import "fmt"
+
+type Data struct {
+	nama   string
+	umur   int
+	alamat string
+}
 
 func main() {
 	fmt.Println("Masuk ke repo")
 	fmt.Println("Masuk 2")
+	DataOrang()
+}
+
+func DataOrang() {
+	Budi := Data{
+		nama:   "Budi",
+		umur:   20,
+		alamat: "Jl. Negara",
+	}
+
+	fmt.Println(Budi)
 }
